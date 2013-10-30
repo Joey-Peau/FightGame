a545441e283b50dcb71346eb7f369e866bec2c96
Joey Peau <joey.peau@gmail.com>
2013-10-30 02:43:47 +0100

    a

--- src/Model/Worlds/Worlds.java
+++ src/Model/Worlds/Worlds.java
@@ -1,5 +1,9 @@
 package Model.Worlds;
 
+import java.util.ArrayList;
+
+import Model.Characters.enemy.Enemy;
+
 public class Worlds {
 
 	// current World
@@ -7,6 +11,10 @@ public class Worlds {
 
 	// lvl's Boundaries
 	int levelInf, levelSup;
+	
+	//enemies present in this world
+	ArrayList<Enemy> enemies;
+	
 
 	/**
 	 * Create the world in witch enemies will be between levelInf and levelSup
@@ -21,12 +29,27 @@ public class Worlds {
 		levelSup = bsup;
 	}
 
+	/**
+	 * Minimal level getter
+	 * @return minimal level's world
+	 */
 	public int getInf() {
 		return levelInf;
 	}
-
+/**
+ * Maximal enemies level in this world
+ * @return world's maximal level
+ */
 	public int getSup() {
 		return levelSup;
+	}
+	
+	/**
+	 * Getter for the enemies list
+	 * @return enemies in the world
+	 */
+	public ArrayList<Enemy> getListEnemies(){
+		return enemies;
 	}
 
 }
