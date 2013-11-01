4b7973bfc4b8de981c0d494e0edc6fdf65e85f0c
Joey Peau <joey.peau@gmail.com>
2013-11-01 11:27:32 +0100

    a

---
+++ src/Model/item/Equipment/protection/armor/UltraArmor.java
@@ -0,0 +1,45 @@
+package Model.item.Equipment.protection.armor;
+
+public class UltraArmor extends Armor {
+
+	public UltraArmor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getMagicReduction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getWeaponReduction() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getMerchantProbability(int level) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getLoot() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPrice() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Ultra Armor";
+	}
+
+}
